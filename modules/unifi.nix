c239d7108fc8b4d1bb9bc32e659f00e1d0bc8a06
Nelson Dane <47427072+NelsonDane@users.noreply.github.com>
2026-07-12 19:16:35 -0400

    use new mongodb package
    Signed-off-by: Nelson Dane <47427072+NelsonDane@users.noreply.github.com>

--- modules/unifi.nix
+++ modules/unifi.nix
@@ -1,9 +1,11 @@
-{
-  flake.modules.nixos.unifi = {
+_: {
+  flake.modules.nixos.unifi = { pkgs, ... }: {
     # https://mynixos.com/nixpkgs/options/services.unifi
     services.unifi = {
       enable = true;
       openFirewall = true;
+      # https://github.com/NixOS/nixpkgs/issues/461961
+      mongodbPackage = pkgs.mongodb-ce;
     };
   };
 }
